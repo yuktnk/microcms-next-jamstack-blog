8a952322178b2a6a295747e01a97a703750b78e5
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-07-10 18:16:19 +0900

    microCMSで投稿した記事の一覧取得、表示

---
+++ libs/client.js
@@ -0,0 +1,7 @@
+// libs/client.js
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: "yuktnk-blog",
+  apiKey: process.env.API_KEY, // プロジェクト内でAPIキーを参照する
+});
