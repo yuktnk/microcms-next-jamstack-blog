f3654abad2292bc484db64a3d0e430297039bd0f
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-07-11 17:27:06 +0900

    Update [slug].tsx

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,7 +2,7 @@ export const getStaticProps = async (context) => {
   const slug = context.params?.slug;
   const draftKey = context.previewData?.draftKey;
   const content = await fetch(
-    `https://xxxxxx.microcms.io/api/v1/blog/${slug}${
+    `https://yuktnk-blog.microcms.io/api/v1/blog/${slug}${
       draftKey !== undefined ? `?draftKey=${draftKey}` : ""
     }`,
     { headers: { "X-API-KEY": process.env.apiKey || "" } }
