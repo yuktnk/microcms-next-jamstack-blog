6ce877d06a6232a65188f19bb1703c1505acc2b4
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-07-11 20:56:27 +0900

    [update] 記事一覧、サイドバーをコンポーネント化

---
+++ components/common/Contents.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const Contents = (props) => {
+  let blogContents = props.articles;
+  return (
+    <div className="w-full mr-6">
+      <ul className="flex justify-between flex-wrap">
+        {blogContents.map((article) => (
+          <li key={article.id} className="border-2 w-2/5 mb-4">
+            <Link href={`/blog/${article.id}`}>
+              <a>{article.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Contents;
