2f0b7eabbd1a790740d6e8b5f276310ce0e7fb7c
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-07-18 20:56:50 +0900

    [update] デプロイエラーの解消

--- pages/blog/page/[id].js
+++ pages/blog/page/[id].js
@@ -4,15 +4,14 @@ import { Pagination } from "../../../components/common/index";
 
 const PER_PAGE = 2;
 
-// pages/blog/[id].js
 export default function BlogPageId({ articles, totalCount }) {
   return (
     <div>
       <ul>
-        {articles.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`blog/${blog.id}`}>
-              <a>{blog.title}</a>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <Link href={`blog/${article.id}`}>
+              <a>{article.title}</a>
             </Link>
           </li>
         ))}
@@ -45,30 +44,6 @@ export const getStaticPaths = async () => {
 };
 
 // データを取得
-// export const getStaticProps = async (context) => {
-//   const id = context.params.id;
-
-//   const key = {
-//     headers: { "X-API-KEY": process.env.API_KEY },
-//   };
-
-//   const data = await fetch(
-//     `https://yuktnk-blog.microcms.io/api/v1/blog?offset=${
-//       (id - 1) * 5
-//     }&limit=5`,
-//     key
-//   )
-//     .then((res) => res.json())
-//     .catch(() => null);
-
-//   return {
-//     props: {
-//       blog: data.contents,
-//       totalCount: data.totalCount,
-//     },
-//   };
-// };
-
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const offsetNum = (id - 1) * 2;
