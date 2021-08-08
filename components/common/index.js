43c9c06e1afaba69b19dada6c56838ce0547cf47
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-08-08 23:15:20 +0900

    ビルドエラー解消

--- components/common/index.js
+++ components/common/index.js
@@ -1,5 +1,6 @@
 export { default as About } from "./About";
 export { default as Article } from "./Article";
+export { default as Contact } from "./Contact";
 export { default as Contents } from "./Contents";
 export { default as DynamicPagination } from "./DynamicPagination";
 export { default as Footer } from "./Footer";
