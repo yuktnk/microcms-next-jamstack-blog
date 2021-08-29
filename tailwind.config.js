4cce1c25b995da7fd9dfaa7ad68104a910b7cc7a
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-08-29 20:59:05 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
