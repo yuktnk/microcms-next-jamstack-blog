a5940cfb87ef11237c859cb72e11480dbeb7cad7
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-08-29 21:07:50 +0900

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    // "./src/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
