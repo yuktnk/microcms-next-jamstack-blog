2ac40ae5b6cf5ad9f5d117817d78f45bbb235c2e
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-09-04 19:27:54 +0900

    カテゴリ一覧ページ作成中

--- src/pages/category/[categoryName]/index.js
+++ src/pages/category/[categoryName]/index.js
@@ -1,13 +1,16 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import { client } from "../libs/client";
-import { Contents, Sidebar } from "../components/common/index";
+import Layout, { siteTitle } from "../../../components/layout";
+import { client } from "../../../libs/client";
+import { Contents, Sidebar } from "../../../components/common/index";
 import Card from "@material-ui/core/Card";
+
+import { useRouter } from 'next/router';
 
 
 export default function Home({ articles, categories, totalCount }) {
-  
-  console.log(articles[0].category.id)
+
+  const router = useRouter();
+  console.log(router.pathname);
 
   return (
     <Layout home>
@@ -22,11 +25,21 @@ export default function Home({ articles, categories, totalCount }) {
   );
 }
 
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "categories" });
+  const paths = data.contents.map((content) => `/category/${content.id}`);
+  return { paths, fallback: false };
+};
+
 // データをテンプレートに受け渡す部分の処理
 export const getStaticProps = async () => {
+
   const articlesData = await client.get({
     endpoint: "blog",
-    queries: { limit: 2 },
+    queries: {
+      limit: 2,
+      filters: "category[equals]others"
+    },
   });
   const categories = await client.get({ endpoint: "categories" });
   return {
