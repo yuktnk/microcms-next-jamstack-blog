be325133ca9e6f9a96ad3ccd13b458f34a8a93da
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-09-14 21:10:33 +0900

    GAタグ修正

--- src/libs/gtag.ts
+++ src/libs/gtag.ts
@@ -2,7 +2,7 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || "";
 
 // PV 測定
-https: export const pageview = (url: string): void => {
+export const pageview = (url: string): void => {
   // GA_TRACKING_ID が設定されていない場合は、処理終了
   if (!GA_TRACKING_ID) return;
   window.gtag("config", GA_TRACKING_ID, {
