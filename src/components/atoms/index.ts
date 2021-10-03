a53ff789ee6a11310109797d1ccf80d7617f7558
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-10-03 15:48:02 +0900

    デプロイエラー解消

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,4 @@
-export { default as Footer } from "./layout/Footer";
-export { default as Header } from "./layout/Header";
-export { default as Heading02 } from "./heading/Heading02";
-export { default as Heading03 } from "./heading/Heading03";
+export { default as Footer } from "./Layout/Footer";
+export { default as Header } from "./Layout/Header";
+export { default as Heading02 } from "./Heading/Heading02";
+export { default as Heading03 } from "./Heading/Heading03";
