1ce7164267b0abf2d8a6992de542b66e37597d97
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-10-10 19:09:06 +0900

    フォント変更テスト

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -33,8 +33,8 @@ const Layout = ({ children, home }: { children: React.ReactNode; home?: boolean 
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-
         <style data-href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"></style>
+        <style data-href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"></style>
         <style data-href="https://fonts.googleapis.com/icon?family=Material+Icons"></style>
       </Head>
 
